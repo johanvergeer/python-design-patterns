9d6b2d61ef97fdd24d58914df8889938bdb9ad96
Johan Vergeer <johan.vergeer@ordina.nl>
2020-02-12 13:26:58 +0100

    Configure plugins

--- docs/conf.py
+++ docs/conf.py
@@ -1,52 +1,80 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+from pathlib import Path
 
-# -- Path setup --------------------------------------------------------------
+HOME_DIR = Path.home()
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
-project = 'Python Design Patterns'
-copyright = '2020, Johan Vergeer'
-author = 'Johan Vergeer'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.plantuml",
 ]
+source_suffix = ".rst"
+master_doc = "index"
+project = "Python Design Patterns"
+year = "2020"
+author = "Johan Vergeer"
+copyright = f"{year}, {author}"
+version = release = "0.1.0"
+nitpicky = True
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+pygments_style = "trac"
+templates_path = ["."]
+extlinks = {
+    "issue": ("https://github.com/johanvergeer/python-design-patterns/issues/%s", "#"),
+    "pr": ("https://github.com/johanvergeer/python-design-patterns/pull/%s", "PR #"),
+}
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+rst_epilog = """
+.. role:: raw-html(raw)
+   :format: html
 
+.. |gof_dp| replace:: :raw-html:`<a href="http://www.amazon.com/Design-Patterns-Elements-Reusable-Object-Oriented/dp/0201633612" target="_blank">Design Patterns: Elements of Reusable Object-Oriented Software</a>`
+.. |j2ee_dp| replace:: :raw-html:`<a href="http://www.amazon.com/J2EE-Design-Patterns-William-Crawford/dp/0596004273/ref=sr_1_2" target="_blank">J2EE Design Patterns</a>`
+.. |jdp| replace:: :raw-html:`<a href="https://github.com/iluwatar/java-design-patterns" target="_blank">Java Design Patterns</a>`
+"""
 
-# -- Options for HTML output -------------------------------------------------
+xref_links = {"key": ("link text", "URL")}
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+xref_links.update(
+    {
+        "gof_dp": (
+            "Design Patterns: Elements of Reusable Object-Oriented Software",
+            "http://www.amazon.com/Design-Patterns-Elements-Reusable-Object-Oriented/dp/0201633612",
+        )
+    }
+)
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+if not on_rtd:  # only set the theme if we're building docs locally
+    html_theme = "sphinx_rtd_theme"
+
+else:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    src_dir = os.path.join(os.path.dirname(dir_path), "src")
+    # Append src dir when building on RTD
+    sys.path.insert(0, src_dir)
+
+html_use_smartypants = True
+html_last_updated_fmt = "%b %d, %Y"
+html_split_index = False
+html_sidebars = {
+    "**": ["searchbox.html", "globaltoc.html", "sourcelink.html"],
+}
+html_short_title = f"{project}-{version}"
+
+napoleon_use_ivar = True
+napoleon_use_rtype = False
+napoleon_use_param = False
+
+# PLANT UML
+plantuml = f'java -jar {HOME_DIR.absolute() / "workspace" / "plantuml.jar"}'
